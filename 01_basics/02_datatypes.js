c488aeb4fe29534962b46de28fc9b00e97bad3d8
Manik Mahajan <168090048+iblameManik@users.noreply.github.com>
2024-06-18 16:04:05 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treats all JS code as newer version
+//alert(3+3) //we are using nodejs, not browser
+console.log(3
+    +
+3)
+//code readability should be high
+
+let name="manik"
+let age=19
+let isLoggedIn=true
+let state;
+
+//Primitive Data Types
+//number => 2 to power 53
+//bigInt
+//string => ""
+//boolean=> true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+//object
+
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
