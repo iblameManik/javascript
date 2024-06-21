13d67027f725055a9f520028b1657047c6647764
Manik Mahajan <168090048+iblameManik@users.noreply.github.com>
2024-06-21 19:37:22 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,32 @@
+const name = "Manik"
+const repoCount = 50
+// console.log(name+ repoCount+" Visit");
+
+// console.log(`My name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('manik-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('n'));
+
+const newString = gameName.substring(0,4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-6,4)
+// console.log(anotherString);
+
+const newStringOne = "  manik   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://manik.com/manik%20mahajan"
+
+// console.log(url.replace('%20','-'))
+// console.log(url.includes('manik'));
+
+console.log(gameName.split('-'));
