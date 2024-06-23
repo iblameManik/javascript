661acb9734836d4ce436f7cdf9c3fbfd44f49c91
Manik Mahajan <168090048+iblameManik@users.noreply.github.com>
2024-06-23 18:47:09 +0000

    scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -4,14 +4,52 @@ let a = 300;
 if(true){
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
     var c = 30
 }
 
-for(let i = 0;i < Array.length; i++){
-    const element = array[i];
-}
+// for(let i = 0;i < Array.length; i++){
+//     const element = array[i];
+// }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "Manik"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if(true){
+    const username = "Manik"
+    if(username==="Manik"){
+        website=" Youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// ++++++++++++++ interesting ++++++++++++
+
+addone(5);
+function addone(value){
+    return value + 1
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
