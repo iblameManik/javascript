a40669f1dbbfc0db1b2cb66f167f3c1ee0b51bdb
Manik Mahajan <168090048+iblameManik@users.noreply.github.com>
2024-10-13 19:40:35 +0000

    class

---
+++ 10_classes_and_oops/myClasses.js
@@ -0,0 +1,41 @@
+//ES6
+
+class User{
+    constructor(username,email,password){
+        this.username=username;
+        this.email=email;
+        this.password=password;
+    }
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+    change2Cap(){
+        return `${this.username.toUpperCase()}`
+    }
+}
+
+const Chai = new User("chai","chai@tea.com","786");
+console.log(Chai.encryptPassword());
+console.log(Chai.change2Cap());
+
+//behind the scene
+
+function User(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+
+const tea = new User("tea", "tea@gmail.com", "123")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
+
